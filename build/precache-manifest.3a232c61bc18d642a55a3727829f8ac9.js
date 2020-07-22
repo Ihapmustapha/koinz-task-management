1d97c5ebc9376862fc9bad67cbc1c157e5436583
Ihap Mustapha <ihapmustapha@gmail.com>
2020-07-22 12:15:20 +0200

    pushing app build

---
+++ build/precache-manifest.3a232c61bc18d642a55a3727829f8ac9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc883bb9198094ec5a1ae92663b71d07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f8ce7eab2fbc5d727b9",
+    "url": "/static/css/main.6000223f.chunk.css"
+  },
+  {
+    "revision": "d494f7812cbab19b5d0c",
+    "url": "/static/js/2.118b5bfc.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.118b5bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f8ce7eab2fbc5d727b9",
+    "url": "/static/js/main.eed2ed44.chunk.js"
+  },
+  {
+    "revision": "68e109e94f2b060144c2",
+    "url": "/static/js/runtime-main.389d4231.js"
+  },
+  {
+    "revision": "12b01b1a8de80dd230b9d39235528883",
+    "url": "/static/media/plus.12b01b1a.svg"
+  }
+]);
