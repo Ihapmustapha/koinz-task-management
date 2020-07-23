f1254a71c0bdd9c5744921240b29c29bf49e008b
Ihap Mustapha <ihapmustapha@gmail.com>
2020-07-24 01:14:18 +0200

    refactor: refactoring idbUpdateItem

--- src/services/indexeddb/idbUpdateItem.js
+++ src/services/indexeddb/idbUpdateItem.js
@@ -4,16 +4,30 @@ export const idbUpdateItem = (item) => {
   return new Promise((resolve, reject) => {
     idbConnect()
       .then((db) => {
-        const request = db
-          .transaction(["task-management"], "readwrite")
-          .objectStore("task-management")
-          .put({ ...item });
+        const transaction = db.transaction(["task-management"], "readwrite");
+        const tasks = transaction.objectStore("task-management");
 
-        request.onsuccess = () => {
-          resolve(request.result);
+        // getting item from db
+        const idIndex = tasks.index("id");
+        const getRequest = idIndex.getKey(item.id);
+
+        // on get success
+        getRequest.onsuccess = () => {
+          const id = getRequest.result;
+          // updating item after get
+          const updateRequest = tasks.put(item, id);
+          // on update success
+          updateRequest.onsuccess = () => {
+            resolve(updateRequest.result);
+          };
+          // on update error
+          updateRequest.onerror = (error) => {
+            reject(error);
+          };
         };
 
-        request.onerror = (error) => {
+        // on get error
+        getRequest.onerror = (error) => {
           reject(error);
         };
       })
