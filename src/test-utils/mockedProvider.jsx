8a0c794bd022f25e827b48ed6b1134d85806df0c
Ihap Mustapha <ihapmustapha@gmail.com>
2020-07-24 14:13:54 +0200

    refactor: adding mockedProvider proptypes

--- src/test-utils/mockedProvider.jsx
+++ src/test-utils/mockedProvider.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Provider } from "react-redux";
 import { configureStore } from "../store";
 
 const MockedProvider = ({ initialState, children }) => (
   <Provider store={configureStore(initialState)}>{children}</Provider>
 );
+
+MockedProvider.propTypes = {
+  initialState: PropTypes.shape({}).isRequired,
+  children: PropTypes.arrayOf(PropTypes.node).isRequired,
+};
 
 export default MockedProvider;
